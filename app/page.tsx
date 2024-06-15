5e484351981439c654cae70853461609b8705690
Datu Anuarudin P. Piang II <88268514+daPiang@users.noreply.github.com>
2024-06-15 15:36:37 +0800

    Clean build post server test

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,13 @@ import UserInfo from "@/components/user-info";
 import { getServerAuthSession } from "@/server/auth";
 import { redirect } from "next/navigation";
 
-
 export default async function HomePage() {
-  const authSession = await getServerAuthSession(); //(1) 
+  const authSession = await getServerAuthSession(); //(1)
 
-  return (  
-  <main className="flex items-center justify-center h-screen">
-    {authSession?.user && <UserInfo user={authSession?.user} />}
-    {!authSession?.user && (
-        `${redirect('/login')}`
-    )}
-  </main>
+  return (
+    <main className='flex items-center justify-center h-screen'>
+      {authSession?.user && <UserInfo user={authSession?.user} />}
+      {!authSession?.user && `${redirect("/login")}`}
+    </main>
   );
 }
